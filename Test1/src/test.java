f96bdea5b67ba7f7109a980f3185c3ec7dc7b885
Sundbddn <51303626+Sundbddn@users.noreply.github.com>
2019-06-03 11:13:32 +0800

    Update test.java

--- Test1/src/test.java
+++ Test1/src/test.java
@@ -1,6 +1,9 @@
 
 public class test {
       public void main(String[] args){
-            System.out.println("嘻嘻哈哈孙哲大傻子");
+            System.out.println("嘻嘻哈哈郭琪大傻子");
+            System.out.println("嘻嘻哈哈张琳懿大傻子");
+            System.out.println("嘻嘻哈哈王玉大傻子");
+            System.out.println("嘻嘻哈哈高岩峰大傻子");
       }
 }
